3d55886a800ed7a80eb3029afca5e4dd708b2073
Bhupendrasinh Thakre <bhupendrasinhthakre@BhupendinhsiMac.attlocal.net>
2019-09-09 11:59:30 -0500

    reading test values

--- first_script.R
+++ first_script.R
@@ -1,3 +1,7 @@
-a = 1
-b= 2
-c= 3
+# Get Libraries
+library(curl)
+library(data.table)
+
+# Get Test Values using data.table
+
+test_values <- fread('https://s3.amazonaws.com/drivendata/data/54/public/test_values.csv')
