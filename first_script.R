9f974fc287f41033331893d415badc8910ee3ade
Bhupendrasinh Thakre <bhupendrasinhthakre@BhupendinhsiMac.attlocal.net>
2019-09-09 22:14:49 -0500

    train values

--- first_script.R
+++ first_script.R
@@ -5,3 +5,7 @@ library(data.table)
 # Get Test Values using data.table
 
 test_values <- fread('https://s3.amazonaws.com/drivendata/data/54/public/test_values.csv')
+
+# Get Train values
+
+train_values <- fread('https://s3.amazonaws.com/drivendata/data/54/public/train_values.csv')
