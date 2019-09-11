9b932ec6cf3b1a5335dae512f1692727baef0fe6
Bhupendrasinh Thakre <bhupendrasinhthakre@BhupendinhsiMac.attlocal.net>
2019-09-10 22:43:22 -0500

    Adding Bar plots through ggplot2

--- start_script.R
+++ start_script.R
@@ -2,6 +2,7 @@
 library(curl)
 library(data.table)
 library(dummies)
+library(ggplot2)
 
 # Get Test Values using data.table
 
@@ -40,3 +41,39 @@ train <- train_1
 # Resting Blood Pressure
 
 hist(train$resting_blood_pressure, xlab = "Resting Blood Pressure", main = 'Resting Blood Pressure', probability = TRUE, labels = TRUE)
+
+# Serum Cholestrol
+
+hist(train$serum_cholesterol_mg_per_dl, xlab = "Serum Cholestrol", main = 'Serum Cholestrol', probability = TRUE, labels = TRUE)
+
+# Age 
+
+hist(train$age, xlab = "Age", main = 'Histogram of Age', probability = TRUE, labels = TRUE)
+
+# Max heart rate achieved
+
+hist(train$max_heart_rate_achieved, xlab = "Heart Rate", main = 'Histogram of Main Heart Achieved', probability = TRUE, labels = TRUE)
+
+# Old Peak Depression
+
+hist(train$oldpeak_eq_st_depression, xlab = "Old Peak Depression", main = 'Histogram of Old Peak Depression', probability = TRUE, labels = TRUE)
+
+# Now to barplots
+# Starting with whether heart disease present or not
+
+ggplot(data = train, aes(x=train$heart_disease_present)) + geom_bar() 
++ geom_text(stat = 'count', aes(label=..count..), vjust=-1) 
++ xlab("Heart Disease Present")
+
+# Quality of Blood Flow to heart
+
+ggplot(data = train, aes(x=train$slope_of_peak_exercise_st_segment)) + geom_bar() 
++ geom_text(stat = 'count', aes(label=..count..), vjust=-1) 
++ xlab("Quality of Blood Flow to Heart")
+
+# Chest Pain Type
+
+ggplot(data = train, aes(x=train$chest_pain_type)) + geom_bar() 
++ geom_text(stat = 'count', aes(label=..count..), vjust=-1) 
++ xlab("Chest Pain Type")
+
